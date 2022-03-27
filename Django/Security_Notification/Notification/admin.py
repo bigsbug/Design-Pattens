c0e36e5cef8f1c9b1ea8d283b6b9dad53b6785d3
bigsbug <booxgitboox@gmail.com>
2022-03-27 20:04:27 +0430

    Register Models

--- Django/Security_Notification/Notification/admin.py
+++ Django/Security_Notification/Notification/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Notification.models import Subscriber_Group_Name, Subscriber_Group
 
 # Register your models here.
+@admin.register(Subscriber_Group)
+class Register_Subscriber_Group(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Subscriber_Group_Name)
+class Register_Subscriber_Group_Name(admin.ModelAdmin):
+    pass
