e1f6b086e665e15307b9e296e92af5c0da3f1494
bigsbug <booxgitboox@gmail.com>
2022-03-27 19:58:52 +0430

    Done models

--- Django/Security_Notification/Notification/models.py
+++ Django/Security_Notification/Notification/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Group_Subscriber(models.Model):
-    ...
+class Group_Name_Subscriber(models.Model):
+    name = models.CharField(
+        verbose_name="Group Name", max_length=21, primary_key=True, unique=True
+    )
 
 
-class Subscriber(models.Model):
-    ...
+class Subscriber_Group(models.Model):
+    group = models.ForeignKey(
+        Group_Name_Subscriber, models.CASCADE, related_name="Subscriber_Group"
+    )
+    uesrs = models.ManyToManyField(User, related_name="Subscriber_Group")
