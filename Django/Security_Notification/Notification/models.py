394c698dc0ddb539066c257f565ea58c3c611575
bigsbug <booxgitboox@gmail.com>
2022-03-28 15:04:15 +0430

    update name of model and add class Meta to models

--- Django/Security_Notification/Notification/models.py
+++ Django/Security_Notification/Notification/models.py
@@ -7,9 +7,17 @@ class Subscriber_Group_Name(models.Model):
         verbose_name="Group Name", max_length=21, primary_key=True, unique=True
     )
 
+    class Meta:
+        verbose_name = "Subscriber Group"
+        verbose_name_plural = "Subscriber Groups"
+
 
 class Subscriber_Group(models.Model):
     group = models.ForeignKey(
         Subscriber_Group_Name, models.CASCADE, related_name="Subscriber_Group"
     )
     uesrs = models.ManyToManyField(User, related_name="Subscriber_Group")
+
+    class Meta:
+        verbose_name = "Subscriber"
+        verbose_name_plural = "Subscribers"
