ca50183a319067067406ac1f40b80a1854712b5e
bigsbug <booxgitboox@gmail.com>
2022-04-05 22:14:13 +0430

    convert strings to f-string

--- Django/strategy/NotificationSystem/Notification/notification_type.py
+++ Django/strategy/NotificationSystem/Notification/notification_type.py
@@ -2,22 +2,22 @@ from Notification.notification_interface import StrategyInterface
 
 
 class MessageInSite(StrategyInterface):
-    def notify(self, reqeust, message, toAll) -> bool:
+    def notify(self, request, message, toAll) -> bool:
         print(
-            "Notification [{message}] to [{request.user}] via Messages in site sended."
+            f"Notification [{message}] to [{request.user}] via Messages in site sended."
         )
 
 
 class Email(StrategyInterface):
-    def notify(self, reqeust, message, toAll) -> bool:
-        print("Notification [{message}] to [{request.user}] via Email sended.")
+    def notify(self, request, message, toAll) -> bool:
+        print(f"Notification [{message}] to [{request.user}] via Email sended.")
 
 
 class Websocket(StrategyInterface):
-    def notify(self, reqeust, message, toAll) -> bool:
-        print("Notification [{message}] to [{request.user}] via Web Socket sended.")
+    def notify(self, request, message, toAll) -> bool:
+        print(f"Notification [{message}] to [{request.user}] via Web Socket sended.")
 
 
 class SMS(StrategyInterface):
-    def notify(self, reqeust, message, toAll) -> bool:
-        print("Notification [{message}] to [{request.user}] via SMS sended.")
+    def notify(self, request, message, toAll) -> bool:
+        print(f"Notification [{message}] to [{request.user}] via SMS sended.")
